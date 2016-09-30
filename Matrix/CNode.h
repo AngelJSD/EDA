09c79824a6673479ec57c2905fbc1b64c53e39df
Fabricio Ballón Cuadros <fballoncuadros@gmail.com>
2016-09-30 03:10:07 -0500

    Correcion insercion

--- Matrix/CNode.h
+++ Matrix/CNode.h
@@ -2,13 +2,17 @@ template<class T>
 struct CNODE
 {
 	T m_data;
+
 	CNODE<T>* m_vertical[2];  // 0 arriba    , 1 abajo
 	CNODE<T>* m_horizontal[2];// 0 izquierda , 1 derecha
-	int m_x,m_y;
-	CNODE(int x,int y,T a)
+
+	int m_x,
+		m_y;
+
+	CNODE(int x, int y, T a)
 	{
-		m_data=a;
-		m_x=x;
-		m_y=y;
+		m_data = a;
+		m_x = x;
+		m_y = y;
 	}
 };
---
+++ Matrix/colors.h
@@ -0,0 +1,19 @@
+#ifndef _COLORS_
+#define _COLORS_
+
+#include <string>
+
+#define reset			"\033[0m"
+#define red				"\033[31m"
+#define green			"\033[32m"
+#define yellow			"\033[33m"
+#define blue			"\033[34m"
+#define magenta			"\033[35m"
+#define cyan			"\033[36m"
+#define white			"\033[37m"
+
+#define bold			"\033[1m"
+#define underline		"\033[4m"
+
+
+#endif	/* _COLORS_ */
