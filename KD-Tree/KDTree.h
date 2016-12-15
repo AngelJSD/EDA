c8f272f0b9eea0a30768844298c7e342d6877038
Marypd97 <mariangela.pacheco@ucsp.edu.pe>
2016-12-15 00:42:03 -0500

    vecino mas cercano

--- KD-Tree/KDTree.h
+++ KD-Tree/KDTree.h
@@ -1,6 +1,6 @@
 #ifndef KD_TREE_KDTREE_H
 #define KD_TREE_KDTREE_H
-
+#include <math.h>
 #include "Node.h"
 
 class cKDTree {
@@ -13,11 +13,13 @@ class cKDTree {
 		virtual ~cKDTree ();
 		void print();
 		void print2(cNode* t, int nivel);
+        float distance(cCoordinate , cCoordinate );
         string vocabulary="xyz";
         cNode* mFindMin(cNode*, int, int);
         cNode* mMinimum(cNode*, cNode*, cNode*, int);
         cNode* mDelete(cCoordinate, cNode *, int);
-
+        cNode* mFindNearestNeighborAux(cCoordinate , cNode**,cNode* ,int, float);
+        cNode* MFindNearestNeighbor(cCoordinate);
 };
 
 #endif //KD_TREE_KDTREE_H
